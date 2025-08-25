626d55a2958fe8173d5c87931c4ceb65cfc7a3a8
Rushabh <bhosalerushabh0@gmail.com>
2025-08-25 15:43:07 +0530

    feat: allow superadmin to assign company admins

--- apps/api/src/models/Company.js
+++ apps/api/src/models/Company.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const CompanySchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    admin: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+    admin: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
   },
   { timestamps: true }
 );
